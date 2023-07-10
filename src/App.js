9ccdc0043d0ef3ca23a1e20b244b3d50d80976b5
IAmMeers <smithl64@uw.edu>
2023-07-10 14:11:09 -0700

    Initializing Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import TestPage from './pages/TestPage';
+import Burger from './pages/Burger';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>MY APP</h1> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TestPage/>}/>
+          <Route path="/burger" element={<Burger/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
