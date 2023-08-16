5023f2a31546f16047ac1b4bcaa77049d2aaf9cf
IAmMeers <smithl64@uw.edu>
2023-08-16 01:42:46 -0700

    Inventory fully editable

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -16,17 +16,63 @@ const Inventory = () => {
     const [categoryFilter, setCategoryFilter] = useState('all');
     const [filteredInventory, setFilteredInventory] = useState([]);
 
-
-    const [newItemOpen, setNewItemOpen] = useState(false);
+    //New item form useStates
+    const [itemOpen, setItemOpen] = useState(false);
     const [formState, setFormState] = useState({
         Item_name: "",
         Quantity: 0,
         Quantity_unit: "",
         Threshold: 0
     });
+    const [errors, setErrors] = useState("");
+
+    //Edit item row useStates
+    const [rowToEdit, setRowToEdit] = useState(null);
     const [isEdit, setEdit] = useState(false);
-    const [disable, setDisable] = useState(true);
-    const [showConfirm, setShowConfirm] = useState(false);
+
+    // const [showConfirm, setShowConfirm] = useState(false); //TODO: Confirm prompt?
+
+    const updateItem = () => {
+        const store_id = params.store_id;
+        const item_name = formState.Item_name;
+        const quantity = formState.Quantity;
+        const quantity_unit = formState.Quantity_unit;
+        const threshold = formState.Threshold;
+
+        console.log("SENDING UPDATE");
+
+        let URL = SERVER_URL + "/inventory/updateItem";
+        const body = {
+            store_id: store_id,
+            item_name: item_name,
+            quantity: quantity,
+            quantity_unit: quantity_unit,
+            threshold: threshold
+        };
+
+        //console.log(body);
+
+        axios.put(URL, body)
+        .then((response) => {
+            console.log(response.data);
+            //Fetch data again (refresh inventory)
+            axios(SERVER_URL + "/Inventory?store_id=" + params.store_id)
+            .then((response) => {
+                console.log(response.data);
+                setInventoryData(response.data["data"]);
+            })
+            .catch((error) => {
+                console.log("Error fetching: \n" + error);
+                console.log(error.response);
+            });
+        })
+        .catch((error) => {
+            console.log("Error fetching: \n" + error);
+            console.log(error.response);
+        });
+
+
+    }
 
 
     function handleDeleteItem(item) {
@@ -58,68 +104,140 @@ const Inventory = () => {
 
     };
 
-    const handleAddItem = (e) => {
+    const handleItemSubmit = (e) => {
         e.preventDefault();
-        console.log(formState);
-        console.log(validateForm());
-        console.log(inventoryData);
-        if (!validateForm()) return;
-            
-        //Add item to inventory DB
-        let URL = SERVER_URL + "/inventory/addItem"
-        const body = {
-            store_id: params.store_id,
-            item_name: formState.Item_name,
-            quantity: formState.Quantity,
-            quantity_unit: formState.Quantity_unit,
-            threshold: formState.Threshold
-        }   
 
-        axios.post(URL, body)
-        .then((response) => {
-            console.log(response.data);
-            //Fetch data again (refresh inventory)
-            axios(SERVER_URL + "/Inventory?store_id=" + params.store_id)
+        if (rowToEdit === null) {
+            if (!validateForm()) return;
+            
+            //Add item to inventory DB
+            let URL = SERVER_URL + "/inventory/addItem"
+            const body = {
+                store_id: params.store_id,
+                item_name: formState.Item_name,
+                quantity: formState.Quantity,
+                quantity_unit: formState.Quantity_unit,
+                threshold: formState.Threshold
+            }   
+    
+            axios.post(URL, body)
             .then((response) => {
                 console.log(response.data);
-                setInventoryData(response.data["data"]);
-                //Reset form
-                setFormState({
-                    Item_name: "",
-                    Quantity: '0',
-                    Quantity_unit: "",
-                    Threshold: '0'
+                //Fetch data again (refresh inventory)
+                axios(SERVER_URL + "/Inventory?store_id=" + params.store_id)
+                .then((response) => {
+                    console.log(response.data);
+                    setInventoryData(response.data["data"]);
+                    //Reset form
+                    setFormState({
+                        Item_name: "",
+                        Quantity: '0',
+                        Quantity_unit: "",
+                        Threshold: '0'
+                    });
+                })
+                .catch((error) => {
+                    console.log("Error fetching: \n" + error);
                 });
             })
             .catch((error) => {
                 console.log("Error fetching: \n" + error);
             });
-        })
-        .catch((error) => {
-            console.log("Error fetching: \n" + error);
-        });
+
+            setItemOpen(false); //Close form
+        } else {
+            inventoryData.map((currRow, index) => {
+                if (currRow === rowToEdit) {
+                    console.log(currRow);
+                    if (validateForm(currRow)) {
+                        //Update item in inventory DB
+                        updateItem();
+                        setItemOpen(false); 
+                    }
+                    
+                }
+            });
+        }
 
 
-        setNewItemOpen(false); 
+        
         
     };
 
-    const validateForm = () => {
-        //Check if all fields are filled
-        if (!formState.Item_name || !formState.Quantity || !formState.Quantity_unit || !formState.Threshold) return false;
-        //Check if quantity and threshold are numbers
-        if (isNaN(formState.Quantity) || isNaN(formState.Threshold)) return false;
-        //Check if quantity and threshold are positive
-        if (formState.Quantity < 0 || formState.Threshold < 0) return false;
-        //Check if item_name is under 50 characters
-        if (formState.Item_name.length > 50) return false;
-        //Check if quantity_unit is under 15 characters
-        if (formState.Quantity_unit.length > 15) return false;
+    const handleEditRow = (index) => {
+        setRowToEdit(index);
+        setEdit(true);
+        setFormState({
+            Item_name: index.Item_name,
+            Quantity: index.Quantity,
+            Quantity_unit: index.Quantity_unit,
+            Threshold: index.Threshold
+        });
+        setItemOpen(true);
         
+    }
+
+    const validateForm = (item=null) => {
+        
+        console.log(item);
+        console.log(isEdit);
+        console.log(formState);
+        //Check Item_name is unchanged
+        if (isEdit && item !== null) {
+
+            if (item.Item_name !== formState.Item_name) {
+                setErrors("Cannot change item name when editing.");
+                return false;
+            }
+        } else {
+            //Check if item_name is unique
+            for (const item of inventoryData) {
+                if (item.Item_name === formState.Item_name) {
+                    setErrors("Item name already exists");
+                    return false;
+                }
+            }
+        }
+        
+        //Check if all fields are filled
+        if (!formState.Item_name || formState.Quantity === '' || !formState.Quantity_unit || formState.Threshold === '') {
+            setErrors("Please fill out all fields");
+            return false;
+        }
+         
+        //Check if quantity and threshold are numbers
+        if (isNaN(formState.Quantity) || isNaN(formState.Threshold)) {
+            setErrors("Quantity or Threshold is not a number");
+            return false;
+        }
+        //Check if item_name and quantity_unit are strings
+        // if (isNaN(formState.Item_name || typeof formState.Quantity_unit !== 'string') {
+        //     setErrors("Item name or Quantity Unit is not a string");
+        //     return false;
+        // }
+        //Check if quantity and threshold are positive
+        if (formState.Quantity < 0 || formState.Threshold < 0) {
+            setErrors("No negative numbers");
+            return false;
+        }
+        //Check if item_name is under 50 characters
+        if (formState.Item_name.length > 50) {
+            setErrors("Item name must be under 50 characters");
+            return false;
+        }
+        //Check if quantity_unit is under 15 characters
+        if (formState.Quantity_unit.length > 15) {
+            setErrors("Quantity Unit must be under 15 characters");
+            return false;
+        }
+        
+        setErrors("");
         return true;
+
 
     }
 
+    //Handling form changes
     const handleChange = (e) => {
         setFormState({
             ...formState,
@@ -219,7 +337,7 @@ const Inventory = () => {
                                     <td>
                                         <span className='actions'>
                                             <BsFillTrashFill onClick={() => handleDeleteItem(item)}/>
-                                            <BsFillPencilFill />
+                                            <BsFillPencilFill onClick={() => handleEditRow(item)}/>
                                         </span>
                                     </td>
                                 </tr>
@@ -228,12 +346,25 @@ const Inventory = () => {
                     </table>
 
                 </section>
-                <button className='btn' onClick={() => setNewItemOpen(true)}>Add</button>
+                <button className='btn' onClick={() => {
+
+                    setItemOpen(true); 
+                    setEdit(false); 
+                    setRowToEdit(null);
+                    setFormState({
+                        Item_name: "",
+                        Quantity: '0',
+                        Quantity_unit: "",
+                        Threshold: '0'
+                    });
+                
+                }
+                }>Add</button>
             </div>
-            {newItemOpen &&   
+            {itemOpen &&   
             <div className="new-item-container" onClick={(e) => {
                 if(e.target.className === 'new-item-container')
-                setNewItemOpen(false)
+                setItemOpen(false)
             }}>
                 <div className="new-item" >
                     <form >
@@ -253,7 +384,8 @@ const Inventory = () => {
                             <label htmlFor="Threshold">Threshold</label>
                             <input name="Threshold" value={formState.Threshold} onChange={handleChange}/>
                         </div>
-                        <button type="submit" className='btn' onClick={handleAddItem}>Submit</button>
+                        {errors && <div className='error'>{errors}</div>}
+                        <button type="submit" className='btn' onClick={handleItemSubmit}>Submit</button>
                     </form>
                 </div>
             </div>
